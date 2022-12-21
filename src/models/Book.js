84a201ba0f903ea2e717b636f92f4da52f6fd87b
Nick Alberts <nwalberts5@gmail.com>
2022-12-21 14:44:40 -0500

    base

---
+++ src/models/Book.js
@@ -0,0 +1,49 @@
+import fs from "fs"
+
+const booksPath = "books.json"
+
+class Book {
+  constructor({ title }) {
+    this.title = title
+  }
+
+  static findAll() {
+    const bookData = JSON.parse(fs.readFileSync(booksPath)).books
+    let books = []
+    bookData.forEach(book => {
+      const newBook = new Book(book)
+      books.push(newBook)
+    })
+    return books
+  }
+
+  isValid() {
+    this.errors = {}
+    const requiredFields = ["title"]
+    let isValid = true
+
+    for (const requiredField of requiredFields) {
+      this.errors[requiredField] = []
+      if (!this[requiredField]) {
+        isValid = false
+        this.errors[requiredField].push("Can't be blank")
+      }
+    }
+    return isValid
+  }
+
+  save() {
+    if (this.isValid()) {
+      delete this.errors
+      const books = Book.findAll()
+      books.push(this)
+      const data = { books: books }
+      fs.writeFileSync(booksPath, JSON.stringify(data))
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+export default Book
